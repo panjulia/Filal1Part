dc4bfd3cccfd3f54f4dd98e9efc48627f960dd8d
Julia <pankova@cuplisurveys.com>
2022-10-09 01:14:31 +0300

    задача13

---
+++ Sem2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Clear();
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine()!);
+if (num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (num >= 1000)
+    {
+        num = num / 10;
+    }
+    num = num % 10;
+    Console.WriteLine(num);
+}
+
+
+
