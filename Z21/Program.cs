f9e1c3160f57ae9377e5d86160396383b778a755
Julia <pankova@cuplisurveys.com>
2022-10-13 02:04:22 +0300

    задача21

--- Z21/Program.cs
+++ Z21/Program.cs
@@ -1,22 +1,28 @@
 ﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double Dist(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double result;
+    result = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+
+    return result;
+}
 Console.Clear();
 Console.WriteLine("Введите координаду Х точки А:");
-int x1 = int.Parse(Console.ReadLine()!);
+int ax = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите координаду Y точки А:");
-int y1 = int.Parse(Console.ReadLine()!);
+int ay = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите координаду Z точки А:");
-int z1 = int.Parse(Console.ReadLine()!);
+int az = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите координаду Х точки B:");
-int x2 = int.Parse(Console.ReadLine()!);
+int bx = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите координаду Y точки А:");
-int y2 = int.Parse(Console.ReadLine()!);
+int by = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите координаду Z точки А:");
-int z2 = int.Parse(Console.ReadLine()!);
+int bz = int.Parse(Console.ReadLine()!);
 
-double result;
-result = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
-Console.WriteLine($"Длина отрезка равна: {result:f2}");
+Console.WriteLine($"Длина отрезка равна: {Dist(ax, ay, az, bx, by, bz):f2}");
 
 
