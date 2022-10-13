28b3815009f3643414077a0cc5c21468623a5544
Julia <pankova@cuplisurveys.com>
2022-10-13 19:36:22 +0300

    задача23

---
+++ Z23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+void Kub(int n)
+{
+   for (int i = 1; i <=n; i++)
+   {
+    Console.Write($"{i*i*i}");
+    if (i != n)
+    {
+        Console.Write(", ");
+    }
+   }
+   }
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int N = int.Parse(Console.ReadLine()!);
+Kub(N);
