475df7ef6769486fa7ca68b313b1e3a8c5347167
Julia <pankova@cuplisurveys.com>
2022-10-14 23:56:21 +0300

    задача25

---
+++ Z25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+double Pow(int a, int b)
+{
+ double n;
+     n = Math.Pow(a, b);
+return n;
+}
+Console.Clear();
+Console.WriteLine("Введите число А: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число В: ");
+int B = int.Parse(Console.ReadLine()!);
+
+
+double resalt = Pow(A, B);
+Console.WriteLine($"Число А в степени В равно: {Pow(A, B)}");
