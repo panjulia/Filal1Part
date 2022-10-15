c48875501dfd870d6df5fd9cc4cb4a879f119d08
Julia <pankova@cuplisurveys.com>
2022-10-15 20:07:17 +0300

    задача25

--- Z25/Program.cs
+++ Z25/Program.cs
@@ -2,11 +2,12 @@
 // возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-double Pow(int a, int b)
+int Pow(int a, int b)
 {
- double n;
-     n = Math.Pow(a, b);
-return n;
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+        result = result * a;
+    return result;
 }
 Console.Clear();
 Console.WriteLine("Введите число А: ");
@@ -14,6 +15,6 @@ int A = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите число В: ");
 int B = int.Parse(Console.ReadLine()!);
 
+int result = Pow(A, B);
+Console.WriteLine($"Число А в степени В равно: {result}");
 
-double resalt = Pow(A, B);
-Console.WriteLine($"Число А в степени В равно: {Pow(A, B)}");
