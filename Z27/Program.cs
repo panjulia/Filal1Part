8f0c165cc0ffd3969df7452348f908f10b5bdc3b
Julia <pankova@cuplisurveys.com>
2022-10-15 20:47:12 +0300

    задача27

---
+++ Z27/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int Sum(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        sum = sum + (n % 10);
+        n = n / 10;
+    }
+    return sum;
+}
+Console.Clear();
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+
+Console.WriteLine($"сумма цифр в числе {N} равнa: {Sum(N)}");
